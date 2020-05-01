75dc87cb87e658a8306775ff655bc7bc258ef833
ismaeljtl <ismaelteixeira6@gmail.com>
2020-05-01 19:06:27 -0400

    Updates

---
+++ precache-manifest.67b4a697a10936a199a84c2d31ce970a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27895cf30fb46f9d60a06cab4a85e990",
+    "url": "/json-placeholder/index.html"
+  },
+  {
+    "revision": "16e21e8b61d22403aa4d",
+    "url": "/json-placeholder/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6bb5d974ce06f6d86f31",
+    "url": "/json-placeholder/static/css/main.42f94f0d.chunk.css"
+  },
+  {
+    "revision": "16e21e8b61d22403aa4d",
+    "url": "/json-placeholder/static/js/2.fa9cba1f.chunk.js"
+  },
+  {
+    "revision": "570d10197340f0c06eb0c25537fab92d",
+    "url": "/json-placeholder/static/js/2.fa9cba1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bb5d974ce06f6d86f31",
+    "url": "/json-placeholder/static/js/main.981525f0.chunk.js"
+  },
+  {
+    "revision": "aac6a08b64db7e389df9",
+    "url": "/json-placeholder/static/js/runtime-main.6889a530.js"
+  }
+]);
